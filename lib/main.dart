bafedb44462a439bb49b3d7e611cc15fc174fe8b
Chandan Gowda <chandansuresh007@gmail.com>
2021-09-11 09:51:15 +0530

    Developed as tutored

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,44 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: XyloPhoneApp(),
+      ),
+    ),
+  );
+}
 
-class XylophoneApp extends StatelessWidget {
+class XyloPhoneApp extends StatelessWidget {
+  Expanded buildKey({int soundNumber, dynamic color}){
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          final player = AudioCache();
+          player.play("wave$soundNumber.wav");
+        },
+        style: TextButton.styleFrom(backgroundColor: color),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Container(),
-        ),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        buildKey(soundNumber: 1, color: Colors.red),
+        buildKey(soundNumber: 2, color: Colors.deepOrangeAccent),
+        buildKey(soundNumber: 1, color: Colors.yellow),
+        buildKey(soundNumber: 1, color: Colors.green),
+        buildKey(soundNumber: 1, color: Colors.blue),
+        buildKey(soundNumber: 1, color: Colors.indigo),
+        buildKey(soundNumber: 1, color: Colors.purple),
+        buildKey(soundNumber: 1, color: Colors.teal),
+      ],
+
     );
   }
 }
